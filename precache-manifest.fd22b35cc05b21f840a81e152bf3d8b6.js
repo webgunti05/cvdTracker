192d4ee9290313356c9c13f9974ee40aa74a2698
Anji <a.gunti@dickblick.com>
2020-04-20 23:15:19 +0530

    Updates

---
+++ precache-manifest.fd22b35cc05b21f840a81e152bf3d8b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6834ecd94e6c32c194bc42fae72837c0",
+    "url": "//webgunti05.github.io/cvdTracker/index.html"
+  },
+  {
+    "revision": "2ec417a32b688218ef7b",
+    "url": "//webgunti05.github.io/cvdTracker/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "05d70f85cd75d50259c0",
+    "url": "//webgunti05.github.io/cvdTracker/static/css/main.b697f1ca.chunk.css"
+  },
+  {
+    "revision": "2ec417a32b688218ef7b",
+    "url": "//webgunti05.github.io/cvdTracker/static/js/2.eb48ad71.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "//webgunti05.github.io/cvdTracker/static/js/2.eb48ad71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d70f85cd75d50259c0",
+    "url": "//webgunti05.github.io/cvdTracker/static/js/main.fb0d677c.chunk.js"
+  },
+  {
+    "revision": "a86f215ff9699c5ec329",
+    "url": "//webgunti05.github.io/cvdTracker/static/js/runtime-main.2ee95d4f.js"
+  },
+  {
+    "revision": "8fd8852e8553ce1405fc4cef6de7d3a5",
+    "url": "//webgunti05.github.io/cvdTracker/static/media/covid19_logo.8fd8852e.png"
+  }
+]);
